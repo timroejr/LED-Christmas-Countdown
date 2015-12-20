08b523219d5d6125d000c94faa6e92d234324b38
Timothy Roe, Jr <timroejr@msn.com>
2015-12-20 14:38:46 -0800

    Ready for Production

--- main.cc
+++ main.cc
@@ -5,6 +5,21 @@
 //  Created by Timothy Roe Jr. on 12/19/15.
 //  Copyright © 2015 Timothy Roe Jr. All rights reserved.
 //
+//  To Give All the Honor and Glory to God
+//  In the Spirit of Christmas, our Lord and Savior, Jesus Christ
+//
+//  "For unto you is born this day in the city of David a Savior,
+//  who is Christ the Lord." -Luke 2:11 ESV
+//
+//  Copyright (c) 2015-2016 Timothy Roe, Jr. & Roeboat, LLC.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+//  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// --START CODE--
 
 #include <iostream>
 #include <unistd.h>
@@ -18,8 +33,6 @@
 #include <getopt.h>
 #include <stdlib.h>
 #include <fstream>
-
-//#include <Python.h>
 
 using rgb_matrix::GPIO;
 using rgb_matrix::RGBMatrix;
@@ -35,23 +48,9 @@ static void DrawOnCanvas(Canvas *led) {
     std::string command = "python ";
     command += filename;
     system(command.c_str());
-    sleep(1);/*
-    const char *bdf_font_file = "fonts/4x6.bdf";
-    bdf_font_file = strdup(optarg);
-    rgb_matrix::Font font;
-    font.LoadFont(bdf_font_file);
-    if (!font.LoadFont(bdf_font_file)) {
-        fprintf(stderr, "ERROR, Font '%s' not found.\n", bdf_font_file);
-    } else {
-        printf("Font Found");
-    }
-    Color color(255, 0, 0);
-    rgb_matrix::DrawText(led, font, 1, font.baseline(), color, "Hello");*/
-    
+    sleep(1);
     
     int center_x = led->width()/2;
-    int width = led->width();
-    int height = led->height();
     int x1 = center_x;
     int x2 = center_x;
     int y;
@@ -102,6 +101,7 @@ static void DrawOnCanvas(Canvas *led) {
         led->SetPixel(i, y2, 0, 255, 0);
     }
     
+    /*
     led->SetPixel(12, 10, 0, 0, 255);
     led->SetPixel(18, 5, 255, 0, 255);
     led->SetPixel(19, 15, 255, 255, 0);
@@ -111,6 +111,7 @@ static void DrawOnCanvas(Canvas *led) {
     led->SetPixel(14, 7, 87, 250, 23);
     led->SetPixel(12, 10, 234, 0, 234);
     led->SetPixel(15, 13, 234, 98, 76);
+     */
     
 }
 
@@ -154,7 +155,6 @@ int main(int argc, char *argv[]) {
         sleep(10);
         led->Clear();
     }
-    //showTime(led);
     
     printf("Press <RETURN> to Reset Board\n");
     getchar();
